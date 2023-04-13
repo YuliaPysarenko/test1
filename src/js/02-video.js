b9350e025da48511f4d846c17f3e1a962ad46e71
YuliaPysarenko <y.pisarenko@ukr.net>
2023-04-13 17:55:54 +0300

    test1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import VimeoPlayer from "@vimeo/player";
+import LodashThrottle from "lodash.throttle";
+console.log(VimeoPlayer);
+console.log(LodashThrottle);
 
+export const iframe = document.querySelector('iframe');
+export const player = new Vimeo.Player(iframe);
+
+
+export const timeUpdatePlayer = player.on('timeupdate', throttle(onPlay, 1000));
+
+export function onPlay(evt) {
+
+    const message = evt.currentTarget;
+    localStorage.setItem(`videoplayer-current-time`, message);
+
+};
+
+export const setCurrentTimePlayer = player.setCurrentTime(30.456).then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function (error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import LodashThrottle from "lodash.throttle";
 
+
+export const form = document.querySelector(`.feedback-form`);
+// const formInput = document.querySelector(`.feedback-form  input`);
+// const formTextarea = document.querySelector(`.feedback-form textarea`);
+
+
+export const formAddEvtSubmit = form.addEventListener(`submit`, onSubmitForm);
+export const formAddEvtInput = form.addEventListener(`input`, throttle(onAllForm, 500));
+
+
+export const STORAGE_KEY = `feedback-form-state`;
+export const ObjectForm = {};
+
+//     const {
+//     elements: { email, password }
+//   } = event.currentTarget;
+
+populateTextareaInput()
+
+export function onSubmitForm(evt) {
+    evt.preventDefault();
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+export function onAllForm(evt) {
+    const message = evt.target.value;
+    localStorage.setItem(STORAGE_KEY, message);
+
+    const formImailMessage = ObjectForm[evt.target.name] = evt.target.value;
+    const stringifyObjectForm = JSON.stringify(formImailMessage);
+    const parseObjectForm = JSON.parse(stringifyObjectForm);
+
+    console.log(parseObjectForm)
+}
+
+export function populateTextareaInput() {
+    const saveMessage = localStorage.getItem(STORAGE_KEY);
+    if (saveMessage) {
+        form.value = saveMessage;
+    }
+}
